e9038ba6df6c4358e56e5890a4a6c6ed0aff3703
root <1095331784@qq.com>
2021-01-11 06:03:44 -0500

    hhd

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="gitalk-container"></div>
 </template>
+
+<script>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    const gitalk = new Gitalk({
+      clientID: "2ef9e8961eaca3d86802",
+      clientSecret: "2d776abda15a07657410e9847f9c2d42044e15b8",
+      repo: "gitalk-repo", // The repository of store comments,
+      owner: "pig-who-like-bacon",
+      admin: ["pig-who-like-bacon"],
+      id: "comment",
+      title: "hhd3",
+      distractionFreeMode: false // Facebook-like distraction free mode
+    });
+    gitalk.render("gitalk-container");
+    return;
+  }
+};
+</script>
