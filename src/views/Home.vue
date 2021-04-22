7c034f80eb28d1103106305aba60a42b4e5a401e
root <1095331784@qq.com>
2021-04-21 23:05:30 -0400

    hhd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,40 @@
 <template>
+  <div style="width: 100%; height: 200px"></div>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img style="width: 300px; max-width: 30%" src="../assets/logo.png" />
+    <h1>Welcome to My Blog</h1>
+  </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import request from "@/utils/request";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  },
-  data() {
-    const params = {
-      page: 0,
-      size: 9999,
-      enabled: true,
-      sort: "id,asc"
-    };
-    console.log(
-      request({
-        url: "api/tag/show",
-        method: "get",
-        params
-      })
-    );
-    return {};
-  }
+  name: "Home"
 };
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
